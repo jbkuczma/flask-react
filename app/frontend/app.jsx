2e3f9dc366470e55e3a47efdba5fe33945281ae4
James <jbkuczma@buffalo.edu>
2017-05-29 16:28:21 -0400

    no router for now

--- app/frontend/app.jsx
+++ app/frontend/app.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import MainWindow from './components/MainWindow.jsx'
+import User from './components/user/User.jsx'
 
 export default class App extends React.Component {
     
     render() {
         return (
-            <MainWindow name="James" />
+            <MainWindow />
         )
     }
 }
